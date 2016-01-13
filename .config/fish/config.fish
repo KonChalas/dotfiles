ce40559a3a7b3bfff070990e239fa067e9a8d6b8
KonChalas <konchalas@ics.forth.gr>
2016-01-14 00:34:58 +0200

    Replace oh my fish with fisherman

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,33 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH /home/konstantinos/.local/share/omf
+set fisher_home ~/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
+fish_vi_mode
 
-# Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG /home/konstantinos/.config/omf
+function my_vi_bindings
+	fish_vi_key_bindings
+	bind -M insert \cl 'clear; commandline -f repaint'
+end
 
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
+set -g fish_key_bindings my_vi_bindings
+
+alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
+
+alias lock='i3lock -c 000000'
+alias emc='emacsclient -c -a ""'
+
+set -Ux fish_user_paths $fish_user_paths ~/Workspace/gcc-linaro-4.9-2014.11-x86_64_aarch64-linux-gnu/bin ~/go/bin
+
+set -Ux GOPATH ~/go
+
+set -Ux EDITOR vim
+
+set -g theme_display_vi yes
+set -g theme_date_format "+"
+set -g theme_display_git no
+
+# Load fishmarks (http://github.com/techwizrd/fishmarks)
+. ~/.config/fisherman/functions/marks.fish
